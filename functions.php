fa1aa256355e1ecf74c4f630cde7962e137036df
Woodrow Shigeru <WoodrowShigeru@users.noreply.github.com>
2016-07-16 13:29:07 +0200

    Update functions.php
    - added a tree getter logic
    - fixed an erroneous referencing path
    - removed unnecessary function-alias

--- functions.php
+++ functions.php
@@ -2,51 +2,50 @@
 /**
  * InteractiveVarDump
  *
- * @copyright  "Copyright" © 2o16 Apr, by Rafael Cieslik ( woodrow.shigeru@gmx.net )
- * @version    1.0.1
- * @url        https://github.com/WoodrowShigeru/InteractiveVarDump
+ * @copyright  "Copyright" © Apr - Jul 2o16, by Rafael Cieslik ( woodrow.shigeru@gmx.net )
+ * @version    1.1
  */
 
+require_once "InteractiveVarDump_Tree.class.php";
 
-require_once "InteractiveVarDump/InteractiveVarDump_Tree.class.php";
- 
- 
+
 $ivd_initialized = FALSE;
- 
- 
-function ivd( $subject, $pretext = false ) {
- 
-    global $ivd_initialized;
- 
-    if (!$ivd_initialized) {
-         // apply necessary includes.
- 
-        echo '<style type="text/css">';
-            include "ivd.css";
-        echo '</style>';
- 
-        echo '<script type="text/javascript">';
-            include "jquery.js";
-        echo '</script>';
- 
-        echo '<script type="text/javascript">';
-            include "ivd.js";
-        echo '</script>';
- 
-        $ivd_initialized = TRUE;
-    }
- 
-    if ($pretext) {
-        echo $pretext .':<br />';
-    }
- 
-    $tree = new InteractiveVarDump_Tree($subject);
-    echo $tree->display();
+
+
+function ivd( $subject, $pretext = FALSE, $getter = FALSE ) {
+
+	global $ivd_initialized;
+
+	if (!$ivd_initialized) {
+		 // apply necessary includes. Doesn't matter where in the DOM you are
+		 // (in DEV environments).
+
+		echo '<style type="text/css">';
+			include "ivd.css";
+		echo '</style>';
+
+		echo '<script type="text/javascript">';
+			include "jquery.js";
+		echo '</script>';
+
+		echo '<script type="text/javascript">';
+			include "ivd.js";
+		echo '</script>';
+
+		$ivd_initialized = TRUE;
+	}
+
+	if ($pretext) {
+		echo $pretext .':<br />';
+	}
+
+	$tree = new InteractiveVarDump_Tree($subject);
+	if ($getter) {
+		return $tree->get();
+
+	} else {
+		echo $tree->display();
+	}
 }
- 
- 
- // set up some aliases.
-function dump( $subject, $pretext = false ) {
-    ivd($subject, $pretext);
-}
+
 
